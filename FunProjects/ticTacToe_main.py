814641352389a8a30ce87317e009a4aca8d31b73
Anushri Thirumavalavan <anushri6688@gmail.com>
2024-12-06 12:48:21 +0200

    changed few functions and modified the tic tac toe game

--- FunProjects/ticTacToe_main.py
+++ FunProjects/ticTacToe_main.py
@@ -26,9 +26,10 @@ def players():
     elif p1 == "O":
         p2 = "X"
         print("Player2: " + p2)
-    elif p1 != "O" or p1 != "X":
+    else:
         print("Sorry,invalid input. Type X or O")
-        play_game()
+        return play_game()
+    return p1,p2
 
 #Define the player position
 def player_position():
@@ -50,60 +51,56 @@ def player_position():
     board[position] = current_player
     display_board()
 
-#Function to play our tic tac game 
+#Function to play our tic tac game
+    #Check winner
+def check_winner():
+    global game_on
+    if board[0] == board[1] == board[2] != "-":
+        game_on = False
+        print("Congratulations " + board[0]+" you WON!")
+    elif board[3] == board[4] == board[5] != "-":
+        game_on = False
+        print("Congratulations " + board[3]+" you WON!")
+    elif board[6] == board[7] == board[8] != "-":
+        game_on = False
+        print("Congratulations " + board[6]+" you WON!")
+    elif board[0] == board[3] == board[6] != "-":
+        game_on = False
+        print("Congratulations " + board[0]+" you WON!")
+    elif board[1] == board[4] == board[7] != "-":
+        game_on = False
+        print("Congratulations " + board[1]+" you WON!")
+    elif board[2] == board[5] == board[8] != "-":
+        game_on = False
+        print("Congratulations " + board[2]+" you WON!")
+             #Check diagonals if there is a win
+    elif board[0] == board[4] == board[8] != "-":
+        game_on = False
+        print("Congratulations " + board[0]+" you WON!")
+    elif board[2] == board[4] == board[6] != "-":
+        game_on = False
+        print("Congratulations "+ board[6]+" you WON!")
+             #If none of the above, then, it's a tie
+    elif "-" not in board:
+        game_on = False
+        print("It's a Tie")
+        exit()
+
+        #Function to flip player
+def flip_player():
+    global current_player
+    if current_player == "X":
+        current_player = "O"
+    else:
+        current_player = "X"
+ 
 def play_game():
     print("My Tic Tac Toe Game")
     display_board()
-    players()
-    
-  #loop  to flip players untill there is a win
+    p1,p2=players()
     while game_on:
         player_position()
-        
-        #Check winner
-        def check_winner():
-            global game_on
-            #Check rows if there is a win 
-            if board[0] == board[1] == board[2] != "-":
-                game_on = False
-                print("Congratulations " + board[0]+" you WON!")
-            elif board[3] == board[4] == board[5] != "-":
-                game_on = False
-                print("Congratulations " + board[3]+" you WON!")
-            elif board[6] == board[7] == board[8] != "-":
-                game_on = False
-                print("Congratulations " + board[6]+" you WON!")
-             #Check columns if there is a win
-            elif board[0] == board[3] == board[6] != "-":
-                game_on = False
-                print("Congratulations " + board[0]+" you WON!")
-            elif board[1] == board[4] == board[7] != "-":
-                game_on = False
-                print("Congratulations " + board[1]+" you WON!")
-            elif board[2] == board[5] == board[8] != "-":
-                game_on = False
-                print("Congratulations " + board[2]+" you WON!")
-             #Check diagonals if there is a win
-            elif board[0] == board[4] == board[8] != "-":
-                game_on = False
-                print("Congratulations " + board[0]+" you WON!")
-            elif board[2] == board[4] == board[6] != "-":
-                game_on = False
-                print("Congratulations "+ board[6]+" you WON!")
-             #If none of the above, then, it's a tie
-            elif "-" not in board:
-                game_on = False
-                print("It's a Tie")
-                exit()
-
-        #Function to flip player
-        def flip_player():
-            global current_player
-            if current_player == "X":
-                current_player = "O"
-            else:
-                current_player = "X"
-        flip_player()
         check_winner()
-#Play our tic tac game
+        flip_player()
 play_game()
+
